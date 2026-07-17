c77490bd71aa51c2c206582f5dbc366039fbbc8d
ved <ved20750327@gmail.com>
2026-07-17 15:56:34 +0545

    added ApiError class for standardized error handling

---
+++ backend/src/utils/ApiError.ts
@@ -0,0 +1,25 @@
+class ApiError extends Error {
+
+    statusCode: number;
+    message: string = "Something went wrong";
+    errors: any[] = [];
+    stack: string = "";
+    data: any;
+    success: boolean = false;
+
+    constructor(statusCode: number, message?: string, errors?: any[], stack: string = "") {
+        super(message);
+        this.statusCode = statusCode;
+        this.data = null;
+        this.success = false;
+        this.errors = errors || [];
+
+        if (stack) {
+            this.stack = stack;
+        } else {
+            (Error as ErrorConstructor & { captureStackTrace?: (targetObject: object, constructorOpt?: Function) => void }).captureStackTrace?.(this, this.constructor);
+        }
+    }
+}
+
+export default ApiError;
